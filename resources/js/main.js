ce6bcc8a303b535153b6c7bdc39feffdc1337933
Jahid Munshi <jahidmunshi99@gmail.com>
2023-08-19 12:09:09 +0600

    strickybar update

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,7 +1,21 @@
+const navBar = document.querySelector("nav");
 const humbargetbtn = document.querySelector("#humbarger");
 const mainMenu = document.querySelector("#main-menu");
 
+// Stricky Navbar
+window.onscroll = () =>{
+    if(document.documentElement.scrollTop>20){
+        navBar.classList.add("sticky");        
+        }
+        else{
+            navBar.classList.remove("sticky")
+    }
+}
+
+// Humburger Menu
 humbargetbtn.addEventListener('click', ()=>{
     mainMenu.classList.toggle("active");
     // humbargetbtn.classList.toggle("active");
 })
+
+
